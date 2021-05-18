56ed95fabe718dfa7ba568cd1ca0f7587a8a4f94
Modji <jeanbaptiste.cochinard@gmail.com>
2021-05-18 18:36:54 +0200

    UPDATE 65%

--- Entrainement/moyen/SuiteDeConway.java
+++ Entrainement/moyen/SuiteDeConway.java
@@ -10,43 +10,41 @@ class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int R = in.nextInt();                                       // nombre de départ
-        int L = in.nextInt();                                       // nombre d'itération
+        int R = in.nextInt();
+        int L = in.nextInt();
+        String line = R + ""; // initialise le premier
 
-        System.out.println(L==1?R:getResponse(L,R));
-    }
+        for (int i = 0; i < L-1; i++) {
+            System.err.println(line);
+            line = getNext(line);            
+        }
 
-    public static String getResponse(int L, int R){
-        ArrayList<Integer> response = new ArrayList<>();
-        response.add(R);
-
-        // boucle L fois
-        for (int i = 0; i < L ; i++) {
-            ArrayList<Integer> MAJResponse = new ArrayList<>();     // la réponse actualisé
-
-            // Pour chaque boucle
-            for (int j = 0; j < response.size(); j++) {
-                //System.err.println(response);
-                int sum = 1;                                        // cumul trouver
-                int test = response.get(j);                         // le nombre actuel tester
-                boolean check = true;                               // pour savoir quand sortir du while 
-                
-                while( j+1 < response.size() && check ){            // Tant qu'ont à une valeur en j+1 
-                    if ( test == response.get(j+1)){                // Si le j+1 est le meme nombre on ajoute au cumul
-                        sum++;
-                    }else{                                          // sinon on sort du while
-                        check = false;
-                    }
-                    j++;
-                }
-
-                MAJResponse.add(sum);
-                MAJResponse.add(test);
-
-                response = MAJResponse;
+        String response = "";
+        for (int i = 0; i < line.length(); i++) {
+            if (i != line.length()-1){
+                response += line.charAt(i) + " ";
+            }else{
+                response += line.charAt(i);
             }
         }
 
-        return response.toString();
+        System.out.println(L==1?R:response);
+    }
+
+    public static String getNext(String line){
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < line.length(); i++) {
+            int count = 1;
+            char test = line.charAt(i);
+
+            while (i+1 < line.length() && line.charAt(i) == line.charAt(i+1)){
+                count++;
+                i++;
+            }
+            str.append(count + "" + test);
+        }
+
+        return str.toString();
     }
 }
